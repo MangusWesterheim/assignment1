459e6fbe75744077839e5f37602783811bb6486b
Magnus Johannessen <magnuswj@stud.ntnu.no>
2023-03-03 11:34:49 +0100

    add: struct suggestion

--- structs.go
+++ structs.go
@@ -1,1 +1,15 @@
 package assignment1
+
+type University struct {
+	isocode  string   `json:"alpha_two_code"`
+	webPages []string `json:"web_pages"`
+	name     string   `json:"name"`
+	country  string   `json:"country"`
+}
+
+type Countries struct {
+	name      string            `json:"name"`
+	isocode   string            `json:"isocode"`
+	languages map[string]string `json:"languages"`
+	maps      string            `json:"maps"`
+}
